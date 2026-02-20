dd0e005b568e2a56f35c5d0fb480d3fda108c653
Peter Kral <peter.kral@pdfix.net>
2026-02-20 17:22:18 +0100

    fix(action,PVQ-4872): Chosen different docker API (that works and returns data) to retrieve latest tag.

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,4 @@
 CONFIG_FILE = "config.json"
-DOCKER_IMAGE = "pdfix/pdf-accessibility-docling"
+DOCKER_NAMESPACE = "pdfix"
+DOCKER_REPOSITORY = "pdf-accessibility-docling"
+DOCKER_IMAGE = f"{DOCKER_NAMESPACE}/{DOCKER_REPOSITORY}"
